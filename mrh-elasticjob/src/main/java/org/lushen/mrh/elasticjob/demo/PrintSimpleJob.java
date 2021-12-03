7bc79f11cb12b3800beb2a4cd2a4e0c5c9185cca
zfhlm <914589210@qq.com>
2021-12-03 11:20:31 +0800

    elasticjob

---
+++ mrh-elasticjob/src/main/java/org/lushen/mrh/elasticjob/demo/PrintSimpleJob.java
@@ -0,0 +1,28 @@
+package org.lushen.mrh.elasticjob.demo;
+
+import org.apache.shardingsphere.elasticjob.api.ShardingContext;
+import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrintSimpleJob implements SimpleJob {
+
+	@Autowired
+	private TestService testService;
+	
+	@Override
+	public void execute(ShardingContext shardingContext) {
+		
+		System.out.println(shardingContext.getJobName());
+		System.out.println(shardingContext.getJobParameter());
+		System.out.println(shardingContext.getShardingItem());
+		System.out.println(shardingContext.getShardingParameter());
+		System.out.println(shardingContext.getShardingTotalCount());
+		System.out.println(shardingContext.getTaskId());
+		
+		testService.test();
+		
+	}
+
+}
---
+++ mrh-elasticjob/src/main/java/org/lushen/mrh/elasticjob/demo/TestService.java
@@ -0,0 +1,12 @@
+package org.lushen.mrh.elasticjob.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+	public void test() {
+		System.out.println("test");
+	}
+
+}
