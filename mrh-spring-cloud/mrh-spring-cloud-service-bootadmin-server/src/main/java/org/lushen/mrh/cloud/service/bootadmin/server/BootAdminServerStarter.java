4ca1e4ba745fb0fa0937b6854f233bd6f2548995
zfhlm <914589210@qq.com>
2022-07-10 01:41:10 +0800

    spring cloud

--- mrh-spring-cloud/mrh-spring-cloud-service-bootadmin-server/src/main/java/org/lushen/mrh/cloud/service/bootadmin/server/BootAdminServerStarter.java
+++ mrh-spring-cloud/mrh-spring-cloud-service-bootadmin-server/src/main/java/org/lushen/mrh/cloud/service/bootadmin/server/BootAdminServerStarter.java
@@ -2,15 +2,13 @@ package org.lushen.mrh.cloud.service.bootadmin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
-@EnableAdminServer
-@EnableDiscoveryClient
 @ComponentScan(basePackageClasses=BootAdminServerStarter.class)
+@EnableAdminServer
 public class BootAdminServerStarter {
 
 	public static void main(String[] args) {
