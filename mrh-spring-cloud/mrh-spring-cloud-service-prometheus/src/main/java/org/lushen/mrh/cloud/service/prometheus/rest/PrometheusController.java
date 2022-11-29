c6b12a1b16231813a44f7308dfe865c2b1326c7e
liming <914589210@qq.com>
2022-11-29 23:13:37 +0800

    spring cloud

--- mrh-spring-cloud/mrh-spring-cloud-service-prometheus/src/main/java/org/lushen/mrh/cloud/service/prometheus/rest/PrometheusController.java
+++ mrh-spring-cloud/mrh-spring-cloud-service-prometheus/src/main/java/org/lushen/mrh/cloud/service/prometheus/rest/PrometheusController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.lushen.mrh.cloud.service.prometheus.rest.PrometheusHttpSdConfigController.HttpSdConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
